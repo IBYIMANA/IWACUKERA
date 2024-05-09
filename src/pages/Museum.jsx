4894fa1471d5a866d2fd4932f7759752f91d4df3
IBYIMANA <filletteibyimana@gmail.com>
2024-05-09 16:26:53 +0200

    dere

--- src/pages/Museum.jsx
+++ src/pages/Museum.jsx
@@ -4,7 +4,15 @@ const Museum = () => {
   return (
     <div>
         <History />
-        Museum
+        <div className='px-2 py-40 '>
+          <div className='absolute px-10 py-5 text-6xl font-extrabold tracking-tight text-white'>
+          <h1> INZU NDANGA MURANGE</h1>
+          </div>
+          <img src="../public/Rwanda.jpg" className="w-full"/>
+          </div>
+          <div>
+            
+          </div>  
         </div>
   )
 }

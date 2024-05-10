9c24b29dcda960e17192c68c82bd0b00371635fe
Mlouise99 <mwubahalouise99@gmail.com>
2024-05-10 15:32:57 +0200

    working on Abami

--- src/pages/Abami.jsx
+++ src/pages/Abami.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import History from './History'
 const Abami = () => {
-  return (
-    <div>
-        <History/>
-        <div className='h-screen py-60 px-60 '>
-          <h1 className='text-4xl font-extrabold text-center text-black'>AMATEKA Y'ABAMI B'URWANDA </h1>
-          <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1'>
-          <img src='../public/umwami1.jpg'/>
-          <h1>1. Gihanga I Ngomijana (1091-1124)</h1>
-          </div>
-          
-          </div>  
-    </div>
-  )
+    return (
+        <div>
+            <History />
+            <div className='h-screen  '>
+                <div className='py-60 px-60'>
+                <h1 className='text-4xl font-extrabold text-center text-black mb-7 pb-3'>AMATEKA Y'ABAMI B'URWANDA </h1>
+                <p className='justify-center items-stretch'>U Rwanda rwagiye rutegekwa n’abami batandukanye kuva rwabaho kugeza ubwo abanyamateka babagabanyamo ibyiciro bitatu bakurikije ibihe aribyo: Abami b’ibimanuka, abami b’umushumi n’abami b’ibitekerezo.
+                    Abami b’Ibimanuka ni abakomoka kuri Shyerezo, Abami b’umushumi ni abatazwi igihe bategekeye n’ibyabaye ku Ngoma zabo, naho Abami b’Ibitekerezo ni Abami bazwi neza igihe bategekeye n’ibyabaye ku ngoma zabo.
+                    Abami b’Ibimanuka bahera kuri Shyerezo bakagera kuri Kazi.
+                  Naho Abami b’umushumi bahera kuri Gihanga cyahanze Inka n’Ingoma bakagera kuri Cyilima I Rugwe. Abami b’Ibitekerezo ari nabo ba nyuma bahera kuri Kigeli I Mukobanya bakageza ku Mwami wa nyuma w’u Rwanda ariwe Kigeli V Ndahindurwa.</p>
+                  </div>
+                <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1'>
+                   
+                    <h1>1. Gihanga I Ngomijana (1091-1124)</h1>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 export default Abami

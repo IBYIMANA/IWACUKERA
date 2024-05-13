12d625c309ab3218acc8c55412d241403d3e6b4c
IBYIMANA <filletteibyimana@gmail.com>
2024-05-13 10:59:47 +0200

    imikino

--- src/pages/Imikino.jsx
+++ src/pages/Imikino.jsx
@@ -4,7 +4,43 @@ const Imikino = () => {
   return (
     <div>
 <History/>
-      Activities
+      <div  className='px-4 py-40'>
+      <h1 className='mb-16 text-4xl font-extrabold text-center text-red-500'>Imikino gakondo mu Rwanda </h1>
+      <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
+      <div className='mt-4 shadow-lg bg-gray-50'>
+        <img src="../public/urukiramende.png" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        </div>
+       
+      </div>
+      <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
+        <div className='mt-4 shadow-lg bg-gray-50'>
+        <img src="../public/umuduri.jpg" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        </div>
+       
+      </div>
+      <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
+        <div className='mt-4 shadow-lg bg-gray-50'>
+        <img src="../public/umugozi.webp" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        </div>
+        <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
+        <div className='mt-4 shadow-lg bg-gray-50'>
+        <img src="../public/kumasha.png" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        </div>
+       
+      </div>
+      <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
+        <div className='mt-4 shadow-lg bg-gray-50'>
+        <img src="../public/kumasha1.jpg" alt="" className='w-1/2 h-auto pt-6 pb-6 rounded-lg shadow-lg' />
+        </div>
+        <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
+        <div className='mt-4 shadow-lg bg-gray-50'>
+        <img src="../public/ibyino.jpg" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        </div>
+       
+      </div>
+      </div>
+      </div>
+      </div>
     </div>
   )
 }

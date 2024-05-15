e86a178423788946917491440742a0c654ee6716
IBYIMANA <filletteibyimana@gmail.com>
2024-05-15 11:13:46 +0200

    imikino complete

--- src/pages/Imikino.jsx
+++ src/pages/Imikino.jsx
@@ -8,13 +8,13 @@ const Imikino = () => {
       <h1 className='mb-16 text-4xl font-extrabold text-center text-red-500'>Imikino gakondo mu Rwanda </h1>
       <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
       <div className='mt-4 shadow-lg bg-gray-50'>
-        <img src="../public/urukiramende.png" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        <img src="../public/urukiramende.png" alt="" className='w-1/2 px-6 pt-6 pb-6 rounded-lg shadow-lg' />
         </div>
        
       </div>
       <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
         <div className='mt-4 shadow-lg bg-gray-50'>
-        <img src="../public/umuduri.jpg" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
+        <img src="../public/umuduri.jpg" alt="" className='w-1/2 px-6 pt-6 pb-6 rounded-lg shadow-lg' />
         </div>
        
       </div>
@@ -26,17 +26,15 @@ const Imikino = () => {
         <div className='mt-4 shadow-lg bg-gray-50'>
         <img src="../public/kumasha.png" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
         </div>
-       
       </div>
       <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
         <div className='mt-4 shadow-lg bg-gray-50'>
-        <img src="../public/kumasha1.jpg" alt="" className='w-1/2 h-auto pt-6 pb-6 rounded-lg shadow-lg' />
+        <img src="../public/kumasha1.jpg" alt="" className='w-1/2 h-32 pt-6 pb-6 rounded-lg shadow-lg' />
         </div>
         <div className='justify-center px-32 rounded-lg shadow-lg bg-red-50 '>
         <div className='mt-4 shadow-lg bg-gray-50'>
         <img src="../public/ibyino.jpg" alt="" className='w-1/2 h-auto px-6 pt-6 pb-6 rounded-lg shadow-lg' />
         </div>
-       
       </div>
       </div>
       </div>
